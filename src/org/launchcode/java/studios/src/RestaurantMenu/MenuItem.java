5c27235524e3dbc40a332ee92b60bcf725e2bbbe
Judy@LAPTOP <judylight2001@gmail.com>
2019-05-16 17:21:35 -0700

    Updated spacing

---
+++ src/org/launchcode/java/studios/src/RestaurantMenu/MenuItem.java
@@ -0,0 +1,49 @@
+package org.launchcode.java.studios.src.RestaurantMenu;
+
+
+public class MenuItem {
+    private double price;
+    private String description;
+    private String category;
+    private boolean isNew;
+
+
+    public MenuItem(double price, String desc, String category, boolean isNew) {
+        this.price = price;
+        this.description = desc;
+        this.category = category;
+        this.isNew = isNew;
+    }
+
+
+    public double getPrice() {
+        return this.price;
+    }
+
+
+    public String getDescription() {
+        return this.description;
+    }
+
+
+    public String getCategory() {
+        return this.category;
+    }
+
+
+    public boolean isNew() {
+        return this.isNew;
+    }
+
+
+    @Override
+    public String toString() {
+        return "MenuItem{" +
+                ", description='" + description + '\'' +
+                "price=" + price +
+                ", category='" + category + '\'' +
+                ", isNew=" + isNew +
+                '}';
+    }
+
+}
